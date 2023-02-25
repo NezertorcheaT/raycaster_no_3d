6cdd09b9d92f4d69b0058d1055edd4f48bfebb2f
NezertorcheaT <tigrisha06ps@gmail.com>
2023-02-25 23:57:38 +0300

    omg

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using NTC.Global.Cache;
 
-public class Player : Entity
+public class Player : MonoCache
 {
-
+    public float speed = 1f;
+    public float speedang = 10f;
+    protected override void Run()
+    {
+        transform.position += transform.up * speed * Input.GetAxisRaw("Vertical");
+        transform.Rotate(new Vector3(0, 0, speedang * Input.GetAxisRaw("Horizontal")));
+    }
 }
--- Assets/Scripts/RaycastCamera.cs
+++ Assets/Scripts/RaycastCamera.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using NTC.Global.Cache;
+using Unity.VisualScripting;
+using UnityEngine.Rendering.RendererUtils;
 
+[AddComponentMenu("Rendering/Raycast Camera")]
 public class RaycastCamera : MonoCache
 {
     [SerializeField] protected LayerMask renderMask;
-    [SerializeField] protected int fov;
-    [SerializeField, Min(2)] protected int wight;
+    [SerializeField] protected int fov = 180;
+    [SerializeField] protected int wight = 5;
+    [SerializeField] protected float clippingFar = 1;
+    [SerializeField] protected Transform canvas;
+    [SerializeField] protected Image canvasImage;
     protected Vector2[] raysList;
+    protected Dictionary<int, SpriteRenderer> rendererList;
 
     private void Awake()
     {
@@ -16,22 +24,49 @@ public class RaycastCamera : MonoCache
     }
     private void UpdateRays()
     {
+        rendererList = new Dictionary<int, SpriteRenderer>();
+        raysList = new Vector2[wight];
         for (int i = 0; i < wight; i++)
         {
-            raysList[i] = new Vector2(Mathf.Cos((360f / i) * Mathf.Deg2Rad), Mathf.Sin((360f / i) * Mathf.Deg2Rad));
+            raysList[i] = new Vector2(Mathf.Cos(((float)fov / (float)wight * (float)i + transform.rotation.eulerAngles.z) * Mathf.Deg2Rad), Mathf.Sin(((float)fov / (float)wight * (float)i + transform.rotation.eulerAngles.z) * Mathf.Deg2Rad));
         }
-    }
-    private void DebugDrawRays()
-    {
-        Gizmos.color = Color.yellow;
-        foreach (var ray in raysList)
+        for (int i = 0; i < wight; i++)
         {
-            Gizmos.DrawRay(new Ray(Vector2.zero, ray));
+            RaycastHit2D rch = Physics2D.Raycast(transform.position, raysList[i], clippingFar, renderMask);
+            if (rch && rch.collider.GetComponent<SpriteRenderer>())
+            {
+                rendererList.Add(i, rch.collider.GetComponent<SpriteRenderer>());
+                raysList[i] = rch.point - (Vector2)transform.position;
+            }
+            else
+                raysList[i] = raysList[i] * clippingFar;
+        }
+
+
+        foreach (Transform child in canvas.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        for (int i = 0; i < wight; i++)
+        {
+            Image cm = Instantiate<Image>(canvasImage);
+            cm.gameObject.transform.SetParent(canvas);
+            if (rendererList.ContainsKey(i))
+                cm.color = rendererList[i].color * (1 / raysList[i].magnitude);
+            else
+                cm.color = Color.black;
+            try
+            {
+                cm.transform.localScale = new Vector2(cm.transform.localScale.x, 1f / raysList[i].magnitude);
+            }
+            catch
+            {
+
+            }
         }
     }
     protected override void Run()
     {
         UpdateRays();
-        DebugDrawRays();
     }
 }
