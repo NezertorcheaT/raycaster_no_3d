f0b4906b0538adc6ad3a4220cc6426f65aa78558
NezertorcheaT <tigrisha06ps@gmail.com>
2023-02-27 18:17:06 +0300

    fuf

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,16 @@ public class Player : MonoBehaviour
     [SerializeField] private float speed = 1f;
     [SerializeField] private float speedang = 10f;
 
+    private void Update()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
     private void FixedUpdate()
     {
         transform.position += transform.up * speed * Input.GetAxisRaw("Vertical");
-        transform.Rotate(new Vector3(0, 0, speedang * Input.GetAxisRaw("Horizontal")));
+        transform.position -= transform.right * speed * Input.GetAxisRaw("Horizontal");
+        transform.rotation *= Quaternion.Euler(0, 0, Input.GetAxis("Mouse X") * speedang);
+        //transform.Rotate(new Vector3(0, 0, speedang * Input.GetAxisRaw("Horizontal")));
     }
 }
--- Assets/Scripts/RaycastCamera.cs
+++ Assets/Scripts/RaycastCamera.cs
@@ -14,7 +14,7 @@ public class RaycastCamera : MonoBehaviour
     [SerializeField] private bool drawGizmos = false;
     private Vector2[] raysList;
     private float[] zList;
-    private Dictionary<int, SpriteRenderer> rendererList;
+    private Dictionary<int, Color> colorList;
 
     private void Awake()
     {
@@ -45,14 +45,14 @@ public class RaycastCamera : MonoBehaviour
     }
     private void CastRays()
     {
-        rendererList = new Dictionary<int, SpriteRenderer>();
+        colorList = new Dictionary<int, Color>();
         zList = new float[wight];
         for (int i = 0; i < wight; i++)
         {
             RaycastHit2D rch = Physics2D.Raycast(transform.position, raysList[i], clippingFar, renderMask);
             if (rch && rch.collider.GetComponent<SpriteRenderer>())
             {
-                rendererList.Add(i, rch.collider.GetComponent<SpriteRenderer>());
+                colorList.Add(i, rch.collider.GetComponent<SpriteRenderer>().color);
                 raysList[i] = (rch.point - (Vector2)transform.position);
                 zList[i] = rch.collider.gameObject.transform.localScale.z;
             }
@@ -66,35 +66,31 @@ public class RaycastCamera : MonoBehaviour
         {
             Destroy(child.gameObject);
         }
-        if (raysList.Length != 0 && rendererList.Count != 0)
+        for (
+            int i = 0;
+            i < wight && (raysList.Length != 0 && colorList.Count != 0);
+            i++
+            )
         {
-            for (int i = 0; i < wight; i++)
-            {
-                Image cm = Instantiate<Image>(canvasImage);
-                cm.gameObject.transform.SetParent(canvas);
-                Color color = Color.black;
-                if (rendererList.ContainsKey(i))
-                    color = rendererList[i].color * (1 / raysList[i].magnitude);
+            Image image = Instantiate<Image>(canvasImage);
+            image.gameObject.transform.SetParent(canvas);
+            Color color = Color.black;
+            if (colorList.ContainsKey(i) && raysList[i].magnitude != 0)
+                color = colorList[i] / raysList[i].magnitude;
 
-                color.a = 255;
-                cm.color = color;
-                try
-                {
-                    float scale = ((wight / (2f * Mathf.Tan(fov / 2 * Mathf.Deg2Rad))) * (zList[i] / 100f)) /
-                        (raysList[i].magnitude * Mathf.Cos(
-                                (transform.rotation.eulerAngles.z - Vector2.SignedAngle(new Vector2(0, 1), raysList[i])
-                                ) * Mathf.Deg2Rad
-                            )
-                        );
+            color.a = 255;
+            image.color = color; 
+
+            float scale = ((wight / (2f * Mathf.Tan(fov / 2 * Mathf.Deg2Rad))) * (zList[i] / 100f)) /
+                    (raysList[i].magnitude * Mathf.Cos(
+                            (transform.rotation.eulerAngles.z - Vector2.SignedAngle(new Vector2(0, 1), raysList[i])
+                            ) * Mathf.Deg2Rad
+                        )
+                    );
 
 
-                    cm.transform.localScale = new Vector2(cm.transform.localScale.x, Mathf.Min(scale, 1));
-                }
-                catch
-                {
-
-                }
-            }
+            image.transform.localScale = 
+                new Vector2(image.transform.localScale.x, Mathf.Min(scale, 1));
         }
     }
     private void FixedUpdate()
